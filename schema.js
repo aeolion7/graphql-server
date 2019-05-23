6fa2ef672bbeb61ad0d1d0af7dc9d9254badbfaa
aeolion7 <joseph.lionarons@cedrus.digital>
2019-05-23 16:54:27 -0400

    create, update, and delete mutations

--- schema.js
+++ schema.js
@@ -45,6 +45,61 @@ const RootQuery = new GraphQLObjectType({
   },
 });
 
+// Mutations (create, update, delete)
+const mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    addCustomer: {
+      type: CustomerType,
+      args: {
+        name: { type: GraphQLNonNull(GraphQLString) },
+        id: { type: GraphQLString },
+        email: { type: GraphQLNonNull(GraphQLString) },
+        age: { type: GraphQLNonNull(GraphQLInt) },
+      },
+      resolve(parent, args) {
+        const { name, email, age } = args;
+        return axios
+          .post(jsonURL, {
+            name,
+            email,
+            age,
+          })
+          .then(res => res.data);
+      },
+    },
+    editCustomer: {
+      type: CustomerType,
+      args: {
+        name: { type: GraphQLString },
+        id: { type: new GraphQLNonNull(GraphQLString) },
+        email: { type: GraphQLString },
+        age: { type: GraphQLInt },
+      },
+      resolve(parent, args) {
+        return axios.patch(jsonURL + `/${args.id}`, args).then(res => res.data);
+      },
+    },
+    deleteCustomer: {
+      type: CustomerType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) },
+      },
+      resolve(parent, args) {
+        const { name, email, age } = args;
+        return axios
+          .delete(jsonURL + `/${args.id}`, {
+            name,
+            email,
+            age,
+          })
+          .then(res => res.data);
+      },
+    },
+  },
+});
+
 module.exports = new GraphQLSchema({
   query: RootQuery,
+  mutation,
 });
