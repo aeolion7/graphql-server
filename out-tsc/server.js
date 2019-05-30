5e33b8eb9a87da6092b5887ef7bc69d3ca1ecae6
aeolion7 <joseph.lionarons@cedrus.digital>
2019-05-30 15:50:07 -0400

    beginning typescript conversion

---
+++ out-tsc/server.js
@@ -0,0 +1,15 @@
+'use strict';
+var express = require('express');
+var expressGraphQL = require('express-graphql');
+var schema = require('./schema');
+var app = express();
+app.use(
+  '/graphql',
+  expressGraphQL({
+    schema: schema,
+    graphiql: true,
+  })
+);
+app.listen(4000, function() {
+  console.log('Application running on port 4000');
+});
